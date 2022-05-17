d70a17604fc5e4ee762f994fa6e3e26d511f6ccf
ekim029 <eric0909kim@gmail.com>
2022-05-17 13:11:24 -0400

    starts game and receives bounds

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -1,1 +1,28 @@
+import random
+
+print("Hello!")
+play = input("Would you like to play the number guessing game? ")
+
+if (play.lower() == "yes" or play.lower() == 'y'):
+    while True:
+        try:
+            lower_bound = int(input("Please input the lower bound "))
+            break
+        except:
+            print("That's not a number...")
+        
+    while True:
+        try:
+            upper_bound = int(input("Please input the upper bound "))
+            if (upper_bound < lower_bound):
+                print("That's not a valid input...")
+                continue
+            break
+        except:
+            print("That's not a number...")
+
+else:
+    quit()
+
+
 
